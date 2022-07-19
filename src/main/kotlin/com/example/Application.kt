46c2ea750743cd4eeda32bc090c5f30e2b0ebbfa
Anton Nasonov <a.nasonov@cmsplanet.ru>
2022-07-19 04:23:54 -0700

    tried to use Netty engine server without embedded

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -2,20 +2,32 @@ package com.example
 
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
 import org.jetbrains.exposed.sql.Database
 
-fun main() {
+fun main(args: Array<String>): Unit {
 //    val config = HikariConfig("hikari.properties")
 //    val dataSource = HikariDataSource(config)
 //    Database.connect(dataSource)
 
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
-        configureRouting()
-        configureSerialization()
-        configureMonitoring()
-        configureSecurity()
-    }.start(wait = true)
+//    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
+//        configureRouting()
+//        configureSerialization()
+//        configureMonitoring()
+//        configureSecurity()
+//    }.start(wait = true)
+
+    EngineMain.main(args)
 }
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
+    configureRouting()
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity()
+}
+
