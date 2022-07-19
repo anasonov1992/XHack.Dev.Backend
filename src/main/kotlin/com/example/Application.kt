ef43a3e52e106dbc5fdad26621f96a121d3107b2
Anton Nasonov <a.nasonov@cmsplanet.ru>
2022-07-19 12:06:58 -0700

    changed PostgreSql driver class name

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -12,12 +12,7 @@ import org.jetbrains.exposed.sql.Database
 
 fun main() {
     val config = HikariConfig("hikari.properties")
-    config.driverClassName = "org.h2.Driver"
-    config.jdbcUrl = "jdbc:h2:file:~/documents/db/h2db"
-    config.maximumPoolSize = 3
-    config.isAutoCommit = false
-    config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
-    config.validate()
+    config.driverClassName = "org.postgresql.Driver"
 
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
