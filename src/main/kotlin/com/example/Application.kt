899df104e95e3fa727388dbce0d6878acc5bba47
Anton Nasonov <a.nasonov@cmsplanet.ru>
2022-07-21 14:27:47 -0700

    set app configuration

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -4,13 +4,21 @@ import com.example.plugins.configureMonitoring
 import com.example.plugins.configureRouting
 import com.example.plugins.configureSecurity
 import com.example.plugins.configureSerialization
+import com.example.security.hashing.SHA256HashingService
+import com.example.security.token.JwtTokenService
+import com.example.security.token.TokenConfig
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import org.jetbrains.exposed.sql.Database
 
-fun main() {
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // application.conf references the main function. This annotation prevents the IDE from marking it as unused.
+fun Application.module() {
     val config = HikariConfig()
     config.driverClassName = "org.postgresql.Driver"
     config.jdbcUrl = "jdbc:postgresql:dcr8kqm6d9nscv?user=ctxdkttailuhtc&password=f353769805bc3738d9103b8c79de0c663d3e2fd477f88d24a6d26800f905169c&ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory"
@@ -22,11 +30,18 @@ fun main() {
     val dataSource = HikariDataSource(config)
     Database.connect(dataSource)
 
-    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
-        configureRouting()
-        configureSerialization()
-        configureMonitoring()
-        configureSecurity()
-    }.start(wait = true)
+//    val userDataSource = MongoUserDataSource(db)
+    val tokenService = JwtTokenService()
+    val tokenConfig = TokenConfig(
+        issuer = environment.config.property("jwt.issuer").getString(),
+        audience = environment.config.property("jwt.audience").getString(),
+        expiresIn = 365L * 1000L * 60L * 60L * 24L,
+        secret = System.getenv("JWT_SECRET"))
+    val hashingService = SHA256HashingService()
+
+    configureRouting(hashingService, tokenService, tokenConfig) //userDataSource,
+    configureSerialization()
+    configureMonitoring()
+    configureSecurity(tokenConfig)
 }
 
