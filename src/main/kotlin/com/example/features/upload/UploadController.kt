eb88b777683901a2180178067abcac6cf2488c69
Anton Nasonov <a.nasonov@cmsplanet.ru>
2022-07-28 16:36:25 -0700

    added uploads files folder creating

--- src/main/kotlin/com/example/features/upload/UploadController.kt
+++ src/main/kotlin/com/example/features/upload/UploadController.kt
@@ -12,53 +12,58 @@ import java.util.*
 
 class UploadController(private val call: ApplicationCall) {
     suspend fun upload() {
+        var fileDescription = ""
+        var filePath = ""
         var fileName = ""
-        val multipart = call.receiveMultipart()
+        val multipartData = call.receiveMultipart()
 
-        multipart.forEachPart { part ->
-            // if part is a file (could be form item)
-            if(part is PartData.FileItem) {
-                // retrieve file name of upload
-                fileName = part.originalFileName!!
-                val file = File("/${Constants.UPLOAD_PATH}}/${fileName}")
+        multipartData.forEachPart { part ->
+            when (part) {
+                is PartData.FormItem -> {
+                    fileDescription = part.value
+                }
+                is PartData.FileItem -> {
+                    var fileBytes = part.streamProvider().readBytes()
+                    val fileExtension = part.originalFileName?.takeLastWhile { it != '.' }
+                    fileName = "${UUID.randomUUID()}.$fileExtension"
+                    filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}"
 
-                // use InputStream from part to save file
-                part.streamProvider().use { its ->
-                    // copy the stream to the file with buffering
-                    file.outputStream().buffered().use {
-                        // note that this is blocking
-                        its.copyTo(it)
-                    }
+                    val folder = File(filePath)
+                    folder.mkdir()
+                    File("${filePath}/${fileName}").writeBytes(fileBytes)
+                }
+                else -> {
+                    call.respond(HttpStatusCode.BadRequest, "Wrong uploaded file format")
                 }
             }
-            // make sure to dispose of the part after use to prevent leaks
-            part.dispose()
         }
 
-        val filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}/${fileName}"
-        
-        call.respond(HttpStatusCode.OK, UploadFileResponse(filePath))
+        call.respond(HttpStatusCode.OK, UploadFileResponse("${filePath}/${fileName}"))
 
-//        var fileDescription = ""
-//        var filePath = ""
-//        val multipartData = call.receiveMultipart()
+//        var fileName = ""
+//        val multipart = call.receiveMultipart()
 //
-//        multipartData.forEachPart { part ->
-//            when (part) {
-//                is PartData.FormItem -> {
-//                    fileDescription = part.value
-//                }
-//                is PartData.FileItem -> {
-//                    var fileBytes = part.streamProvider().readBytes()
-//                    val fileExtension = part.originalFileName?.takeLastWhile { it != '.' }
-//                    val fileName = "${UUID.randomUUID()}.$fileExtension"
-//                    val filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}/${fileName}"
-//                    File(filePath).writeBytes(fileBytes)
-//                }
-//                else -> {
-//                    call.respond(HttpStatusCode.BadRequest, "Wrong uploaded file format")
+//        multipart.forEachPart { part ->
+//            // if part is a file (could be form item)
+//            if(part is PartData.FileItem) {
+//                // retrieve file name of upload
+//                fileName = part.originalFileName!!
+//                val file = File("/${Constants.UPLOAD_PATH}}/${fileName}")
+//
+//                // use InputStream from part to save file
+//                part.streamProvider().use { its ->
+//                    // copy the stream to the file with buffering
+//                    file.outputStream().buffered().use {
+//                        // note that this is blocking
+//                        its.copyTo(it)
+//                    }
 //                }
 //            }
+//            // make sure to dispose of the part after use to prevent leaks
+//            part.dispose()
 //        }
+//
+//        val filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}/${fileName}"
+//        call.respond(HttpStatusCode.OK, UploadFileResponse(filePath))
     }
 }
