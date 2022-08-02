12baf47b058c01a178e0affc2a0ef6e54e557889
Anton Nasonov <a.nasonov@cmsplanet.ru>
2022-08-02 12:30:19 -0700

    updated upload file logic, added test image

--- src/main/kotlin/com/example/features/upload/UploadController.kt
+++ src/main/kotlin/com/example/features/upload/UploadController.kt
@@ -8,29 +8,29 @@ import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import java.io.File
-import java.util.*
 
 class UploadController(private val call: ApplicationCall) {
     suspend fun upload() {
-        var fileDescription = ""
         var filePath = ""
         var fileName = ""
         val multipartData = call.receiveMultipart()
 
         multipartData.forEachPart { part ->
             when (part) {
-                is PartData.FormItem -> {
-                    fileDescription = part.value
-                }
                 is PartData.FileItem -> {
-                    var fileBytes = part.streamProvider().readBytes()
-                    val fileExtension = part.originalFileName?.takeLastWhile { it != '.' }
-                    fileName = "${UUID.randomUUID()}.$fileExtension"
-                    filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}"
+                    if (part.originalFileName == null) {
+                        call.respond(HttpStatusCode.BadRequest, "Missing uploaded file name")
+                    }
+                    else {
+                        part.streamProvider().use {
+                            var fileBytes = it.readBytes()
+                            fileName = part.originalFileName!!
+                            filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}"
+                            File("${filePath}/${fileName}").writeBytes(fileBytes)
+                        }
+                    }
 
-                    val folder = File(filePath)
-                    folder.mkdir()
-                    File("${filePath}/${fileName}").writeBytes(fileBytes)
+                    part.dispose()
                 }
                 else -> {
                     call.respond(HttpStatusCode.BadRequest, "Wrong uploaded file format")
@@ -39,31 +39,5 @@ class UploadController(private val call: ApplicationCall) {
         }
 
         call.respond(HttpStatusCode.OK, UploadFileResponse("${filePath}/${fileName}"))
-
-//        var fileName = ""
-//        val multipart = call.receiveMultipart()
-//
-//        multipart.forEachPart { part ->
-//            // if part is a file (could be form item)
-//            if(part is PartData.FileItem) {
-//                // retrieve file name of upload
-//                fileName = part.originalFileName!!
-//                val file = File("/${Constants.UPLOAD_PATH}}/${fileName}")
-//
-//                // use InputStream from part to save file
-//                part.streamProvider().use { its ->
-//                    // copy the stream to the file with buffering
-//                    file.outputStream().buffered().use {
-//                        // note that this is blocking
-//                        its.copyTo(it)
-//                    }
-//                }
-//            }
-//            // make sure to dispose of the part after use to prevent leaks
-//            part.dispose()
-//        }
-//
-//        val filePath = "${Constants.BASE_URL}/${Constants.UPLOAD_PATH}/${fileName}"
-//        call.respond(HttpStatusCode.OK, UploadFileResponse(filePath))
     }
 }
