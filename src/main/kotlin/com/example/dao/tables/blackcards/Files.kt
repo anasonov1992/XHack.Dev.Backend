cdbad75da4658b1b17af1db4adf81ce1d0fef9b4
Anton Nasonov <anton.nasonov1992@gmail.com>
2022-11-02 21:02:25 +0300

    fixed db table fields names

--- src/main/kotlin/com/example/dao/tables/blackcards/Files.kt
+++ src/main/kotlin/com/example/dao/tables/blackcards/Files.kt
@@ -5,6 +5,6 @@ import org.jetbrains.exposed.dao.id.IntIdTable
 object Files : IntIdTable() {
     val guid = uuid("guid")
     val name = varchar("name", 128)
-    val extension = varchar("name", 32)
-    val path = varchar("name", 128)
+    val extension = varchar("extension", 32)
+    val path = varchar("path", 128)
 }
