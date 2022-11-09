6469db59f850e0a97c63bcb255bc03aa97edd119
Anton Nasonov <anton.nasonov1992@gmail.com>
2022-11-09 14:31:17 +0300

    made 'fractionType' field not nullable

--- src/main/kotlin/com/example/dao/tables/blackcards/Fractions.kt
+++ src/main/kotlin/com/example/dao/tables/blackcards/Fractions.kt
@@ -4,7 +4,7 @@ import com.example.data.primitives.blackcards.FractionType
 import org.jetbrains.exposed.dao.id.IntIdTable
 
 object Fractions : IntIdTable() {
-    val type = enumeration<FractionType>("type").nullable()
+    val type = enumeration<FractionType>("type")
     val name = varchar("name", 128)
     val description = text("description")
     val artUrl = varchar("art_url", 512).nullable()
